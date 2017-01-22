ac80ccb01be2d283ef0bbaeab6282f9ce9e04d23
Jeff Houde <jhoude@outwardinc.com>
2017-01-21 23:40:35 -0800

    reduced the size of the hermit sprites. image cleanup.

--- static/js/entity.js
+++ static/js/entity.js
@@ -54,12 +54,12 @@ function returnDefaultEntities(type) {
   switch(type) {
     case 'PlayerEntity': 
       return {
-        'idle': new Animation( hermitIdleDown, 500, 333, 5),
-        'left': new Animation( hermitWalkRight, 500, 333, 15, true),
-        'right': new Animation( hermitWalkRight, 500, 333, 15, false),
-        'up': new Animation( hermitIdleDown, 500, 333),
-        'down': new Animation( hermitIdleDown, 500, 333, 15, true),
-        'burrow': new Animation( hermitIdleDown, 500, 333, 5, false, false, [1,1,1,1,1,1,1]),
+        'idle': new Animation( hermitIdleDown, 100, 67, 5),
+        'left': new Animation( hermitWalkRight, 100, 67, 15, true),
+        'right': new Animation( hermitWalkRight, 100, 67, 15, false),
+        'up': new Animation( hermitIdleDown, 100, 67),
+        'down': new Animation( hermitIdleDown, 100, 67, 15, true),
+        'burrow': new Animation( hermitIdleDown, 100, 67, 5, false, false, [1,1,1,1,1,1,1]),
       };
       break;
     case 'FoodEntity': 
